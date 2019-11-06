62f57258733db494a1b8db6cedb66b0ce5218cae
elchayshoval <elchayshoval@gmail.com>
2019-11-06 17:33:55 +0200

    add IDL to DL

---
+++ DL/IDL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+namespace DL
+{
+    interface IDL
+    {
+        IEquatable<Store> getStore();
+        bool addStore(Store store);
+        bool removeStore(Store store);
+        bool updateStore(Store store);
+    }
+}
