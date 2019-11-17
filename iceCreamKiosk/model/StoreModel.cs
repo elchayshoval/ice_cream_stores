1b9893101335d63e966ea9226ce4315ec943f0ab
Avremi Cheshin <avremicheshin@gmail.com>
2019-11-17 15:28:06 +0200

    IceCream model and feedback model added

--- iceCreamKiosk/model/StoreModel.cs
+++ iceCreamKiosk/model/StoreModel.cs
@@ -18,7 +18,7 @@ namespace iceCreamKiosk.model
         public List<IceCream> iceCreams { get; set; } = new List<IceCream>();
 
         public string Name { get => name; set => Set(ref name, value); }
-        public string Image { get => image; set => Set(ref image, value); }
+        public string Image { get => image; set => Set(ref image, value); }   
         public string Address { get => address; set => Set(ref address, value); }
         public string Phone { get => phone; set => Set(ref phone, value); }
         public ObservableCollection<IceCream> IceCreams { get ; set; }
